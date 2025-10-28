26f21f70a7f0745f66141f95e42bb1470419a206
Joés <joseataydeleyva@gmail.com>
2025-10-28 14:53:25 -0600

    Se añaden algunos archivos php, y la alta del usuario funciona.

--- php/altaUsuario.php
+++ php/altaUsuario.php
@@ -1,18 +1,24 @@
 <?php
     $dbhost = "localhost";
-    $dbname = "pasteleria";
+    $dbname = "sortec";
     $dbuser = "root";
     $dbpass = "";
     $dbport = "3306";
 
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
+    $telefono = $_POST["telefono"];
     $correo = $_POST["correo"];
     $contrasena = $_POST["contrasena"];
 
     if (($nombre!="") && ($apellido!="") && ($correo!="") && ($contrasena!=""))
     {
-        $sql = "INSERT INTO `usuario` ( `idUsuario` , `nombre` , `correo`, `contrasena` ) VALUES ('$idUsuario', '$nombre', '$correo', '$contrasena');";
+        // Generar idUsuario aleatorio de 10 caracteres
+        $char_string = "01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $length = 10;
+        $idUsuario = substr( str_shuffle( $char_string ), 0, $length );
+
+        $sql = "INSERT INTO `usuario` ( `idUsuario` , `nombre` , `apellido` , `telefono`,  `correo`, `contrasena` ) VALUES ('$idUsuario', '$nombre', '$apellido', '$telefono', '$correo', '$contrasena');";
 
         $conexion = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport) or die("*o*");
         mysqli_query($conexion, "SELECT * FROM usuario");
